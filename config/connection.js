52a765cf88e543c71724bdb8a29765b76686a881
irenebogues <irenebogues@gmail.com>
2019-01-19 12:56:39 -0500

    updating front-end

--- config/connection.js
+++ config/connection.js
@@ -1,34 +1,34 @@
-// *********************************************************************************
-// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
-// *********************************************************************************
+// // *********************************************************************************
+// // CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// // *********************************************************************************
 
-var mysql = require("mysql");
-var keys = require("./keys")
+// var mysql = require("mysql");
 
-// we placed the connections in this source object
-var source = {
-  // localhost
-  localhost: {
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burger_db"
-  },
-
-  
+// // we placed the connections in this source object
+// var source = {
+//   // localhost
+//   localhost: {
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "password",
+//     database: "burger_db"
+//   },
 
   
+//    var connection = mysql.createConnection(keys);
   
-   var connection = mysql.createConnection(keys);
+//    connection.connect(error => {
+//     if (error) {
+//       console.log(error);
+//       return;
+//     };
+//     console.log("Connected as id " + connection.threadId);
+//   });
   
-   connection.connect(error => {
-    if (error) {
-      console.log(error);
-      return;
-    };
-    console.log("Connected as id " + connection.threadId);
-  });
+//    module.exports = connection; 
   
-   module.exports = connection; 
-  
+var mysql = require('mysql');
+var connection;
+
+// add in the environment variable option for JAWSDB for heroku
