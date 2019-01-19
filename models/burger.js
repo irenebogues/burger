cb68c99a4b128bf8fc2ec8da8836510719dedbc9
irenebogues <irenebogues@gmail.com>
2019-01-19 10:39:26 -0500

    updating burger controller

--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,13 @@
-// Event handlers	
-$(function() {	
-    $(".burger-form").on("submit", function(event) {	
-        event.preventDefault();	
+// // Event handlers	
+// $(function() {	
+//     $(".burger-form").on("submit", function(event) {	
+//         event.preventDefault();	
+
+
+var express = require('express');
+var router = express.Router();
+var burger = require('../models/burgers.js');
+
+router.get('/', function(req, res) {
+	res.redirect('/index');
+});
