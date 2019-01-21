907fe3aa5d77074e16e1a777b02629b136b425ce
irenebogues <irenebogues@gmail.com>
2019-01-21 15:24:14 -0500

    updating connections

--- models/burger.js
+++ models/burger.js
@@ -4,10 +4,40 @@
 //         event.preventDefault();	
 
 
-var express = require('express');
-var router = express.Router();
-var burger = require('../models/burgers.js');
+// var express = require('express');
+// var router = express.Router();
+// var burger = require('../models/burgers.js');
 
-router.get('/', function(req, res) {
-	res.redirect('/index');
-});
+// router.get('/', function(req, res) {
+// 	res.redirect('/index');
+// });
+
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var burger = {
+  all: function(cb) {
+    orm.all("burger", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("burger", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burger", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("burger", condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (burger_controller.js).
+module.exports = burger;
